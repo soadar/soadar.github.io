868fd1cb46e17db83c1b898506ad5e7e105dcbd1
Salas <rsalas@bancocredicoop.coop>
2023-04-10 16:28:09 -0300

    .

--- HorasExtras/scripts.js
+++ HorasExtras/scripts.js
@@ -1,8 +1,14 @@
 //Default
-jQuery.datetimepicker.setLocale('es');
+jQuery.datetimepicker.setLocale("es");
 
 var defaultDatetime = "2020-01-01T19:30:00.000Z"; //'2020-03-29 19:30';
 
+if (!JSON.parse(localStorage.getItem("user"))) {
+  alert("Debe iniciar sesion primero");
+  window.location.replace("index.html");
+}
+
+let user = JSON.parse(localStorage.getItem("user")) || [];
 let tasks = JSON.parse(localStorage.getItem("horas")) || [];
 
 if (localStorage.getItem("horas")) {
@@ -10,7 +16,6 @@ if (localStorage.getItem("horas")) {
     crearTarea(task);
   });
 }
-actualizarHoras()
 //console.log(tasks);
 
 let localTzName = moment.tz.guess();
@@ -24,15 +29,20 @@ $("#pickerDateTime1").datetimepicker({
   format: "DD-MM-YYYY", //'dddd MMMM DD, hh:mm A',//'Y-m-d H:i',
   step: 15,
   months: [
-    'Enero', 'Febrero', 'Marzo', 'Abril',
-    'Mayo', 'Junio', 'Julio', 'Agosto',
-    'Septiembre', 'Octubre', 'Noviembre', 'Diciembre',
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
   ],
-  dayOfWeek: [
-    "Dom.", "Lun", "Mar", "Mier",
-    "Jue", "Vier", "Sab",
-  ]
-
+  dayOfWeek: ["Dom.", "Lun", "Mar", "Mier", "Jue", "Vier", "Sab"],
 });
 $("#pickerDateTime2").datetimepicker({
   datepicker: false,
@@ -62,41 +72,67 @@ $("#pickerDateTime5").datetimepicker({
 });
 
 document.getElementById("form").addEventListener("submit", function (event) {
-  event.preventDefault()
+  event.preventDefault();
   //console.log(event.target)
-  var fecha = moment($('#pickerDateTime1').val(), 'DD-MM-YYYY');
+  var fecha = moment($("#pickerDateTime1").val(), "DD-MM-YYYY");
 
-  var HoraExtraEntrada = moment($('#pickerDateTime2').val(), 'HH:mm');
-  var HoraEntrada = moment($('#pickerDateTime3').val(), 'HH:mm');
-  var HoraSalida = moment($('#pickerDateTime4').val(), 'HH:mm');
-  var HoraExtraSalida = moment($('#pickerDateTime5').val(), 'HH:mm');
-
+  var HoraExtraEntrada = moment($("#pickerDateTime2").val(), "HH:mm");
+  var HoraEntrada = moment($("#pickerDateTime3").val(), "HH:mm");
+  var HoraSalida = moment($("#pickerDateTime4").val(), "HH:mm");
+  var HoraExtraSalida = moment($("#pickerDateTime5").val(), "HH:mm");
 
   var diffStart = moment.duration(HoraEntrada.diff(HoraExtraEntrada));
-  diffTotalStart = [diffStart.hours().toString().padStart(2, '0'), diffStart.minutes().toString().padStart(2, '0')].join(':')
+  diffTotalStart = [
+    diffStart.hours().toString().padStart(2, "0"),
+    diffStart.minutes().toString().padStart(2, "0"),
+  ].join(":");
   //console.log(diffTotalStart);
 
   var diffEnd = moment.duration(HoraExtraSalida.diff(HoraSalida));
-  diffTotalEnd = [diffEnd.hours().toString().padStart(2, '0'), diffEnd.minutes().toString().padStart(2, '0')].join(':')
+  diffTotalEnd = [
+    diffEnd.hours().toString().padStart(2, "0"),
+    diffEnd.minutes().toString().padStart(2, "0"),
+  ].join(":");
   //console.log(diffTotalEnd);
 
   diffTotal = diffStart.add(diffTotalEnd);
   //console.log(diffTotal);
 
-  diffTotalTotal = [diffTotal.hours().toString().padStart(2, '0'), diffTotal.minutes().toString().padStart(2, '0')].join(':')
+  diffTotalTotal = [
+    diffTotal.hours().toString().padStart(2, "0"),
+    diffTotal.minutes().toString().padStart(2, "0"),
+  ].join(":");
 
-  const task = {
-    id: new Date().getTime(),
-    dia: fecha._i,
-    horas: diffTotalTotal,
+  //let select = document.createElement('motivoArea');
+  //
+  //console.log(HoraExtraEntrada._i)
+  if (diffTotalTotal != "00:00") {
+    const task = {
+      id: new Date().getTime(),
+      dia: fecha._i,
+      horas: diffTotalTotal,
+      horaEntrada: HoraExtraEntrada._i,
+      horaSalida: HoraExtraSalida._i,
+      motivo: document.getElementById("motivoArea").value,
+      prefijo: prefijo.options[prefijo.selectedIndex].text,
+    };
+    tasks.push(task);
+    //console.log(task);
+    localStorage.setItem("horas", JSON.stringify(tasks));
+    //console.log(JSON.stringify(task));
+    crearTarea(task);
   }
-  tasks.push(task);
-  //console.log(task);
-  localStorage.setItem("horas", JSON.stringify(tasks));
-  //console.log(JSON.stringify(task));
-  crearTarea(task);
-
 });
+
+let prefijo = document.getElementById("prefijo");
+
+//prefijo.addEventListener("change", function() {
+//var mylist = document.getElementById("myList");
+//console.log(prefijo.options[prefijo.selectedIndex].text);
+//var mylist = document.getElementById("myList");
+//document.getElementById("favourite").value =
+//mylist.options[mylist.selectedIndex].text;
+//})
 
 function crearTarea(tarea) {
   const li = document.createElement("li");
@@ -104,7 +140,7 @@ function crearTarea(tarea) {
   li.id = tarea.id;
   let lista = document.getElementById("lista");
   const button = document.createElement("button");
-  button.classList = 'btnX'
+  button.classList = "btnX";
   button.innerHTML = "x";
 
   button.onclick = () => {
@@ -112,12 +148,14 @@ function crearTarea(tarea) {
     tasks = tasks.filter((task) => task.id !== parseInt(button.parentNode.id));
     localStorage.setItem("horas", JSON.stringify(tasks));
     actualizarHoras();
-  }
+  };
   li.appendChild(button);
   lista.appendChild(li);
   form.reset();
   actualizarHoras();
+  console.log(tarea);
 }
+
 let totalHoras = moment.duration();
 
 function actualizarHoras() {
@@ -127,20 +165,22 @@ function actualizarHoras() {
   if (localStorage.getItem("horas")) {
     tasks.map((task) => {
       totalHoras.add(moment.duration(task.horas.toString()));
+      //console.log(task);
     });
     if (totalHoras.hours() > 0 || totalHoras.minutes() > 0) {
-      horas.innerHTML = [totalHoras.hours().toString().padStart(2, '0'), totalHoras.minutes().toString().padStart(2, '0')].join(':');
+      horas.innerHTML = [
+        totalHoras.hours().toString().padStart(2, "0"),
+        totalHoras.minutes().toString().padStart(2, "0"),
+      ].join(":");
       //horas.style.border = "5px solid lightgreen"
       //console.log("1")
     } else {
-      horas.innerHTML = '00:00';
+      horas.innerHTML = "00:00";
       //console.log("2")
     }
   } else {
-    horas.innerHTML = '00:00';
+    horas.innerHTML = "00:00";
   }
-  let user = JSON.parse(localStorage.getItem("user")) || [];
-  horas.innerHTML += " - " + user.charAt(0).toUpperCase() + user.slice(1)
+  horas.innerHTML += " - " + user.charAt(0).toUpperCase() + user.slice(1);
 }
 //console.log(totalHoras);
-
