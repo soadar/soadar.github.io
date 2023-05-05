f696c7bec6ec50fdc3a7dbbfc5f147d98e4030da
soadar <60164893+soadar@users.noreply.github.com>
2023-05-05 02:05:11 -0300

    1

---
+++ DanusEventos/js/scripts.js
@@ -0,0 +1,12 @@
+const year = document.querySelector('#year')
+if (year) {
+    year.innerHTML = new Date().getFullYear();
+}
+
+const form = document.getElementById("form");
+if (form) {
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        console.log("aa");
+    })
+}
